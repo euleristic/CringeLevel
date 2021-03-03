d85bd558cc59e17ae9b0ec2dde17d7a6c1810b2a
euleristic <valter.ottenvang@gmail.com>
2021-03-03 16:48:16 +0100

    Added a guard

--- Level/Assets/Scripts/Patrol.cs
+++ Level/Assets/Scripts/Patrol.cs
@@ -4,25 +4,58 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
+    [SerializeField] PlayerMovement player;
+    [SerializeField] float patrolSpeed;
+    [SerializeField] float chaseSpeed;
+    [SerializeField] float viewAngle;
     CharacterController controller;
     Vector3 gravity;
     Vector3 velocity;
     public List<Vector3> checkPoints;
-    short currentCheckPointIndex;
-    PlayerMovement player;
+    int currentCheckPointIndex;
     Vector3 lastKnownLocation;
+    bool chasing;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         currentCheckPointIndex = 0;
+        chasing = false;
     }
 
     void Update()
     {
-        if (checkPoints.Count != 0)
+        if (!chasing && checkPoints.Count != 0)
         {
+            velocity = (checkPoints[currentCheckPointIndex] - transform.position).normalized * patrolSpeed;
+            if ((checkPoints[currentCheckPointIndex] - transform.position).sqrMagnitude < 0.5f)
+            {
+                currentCheckPointIndex++;
+                currentCheckPointIndex %= checkPoints.Count;
+            }
+        }
+        if (chasing && player != null)
+        {
+            velocity = (lastKnownLocation - transform.position).normalized * chaseSpeed;
+            if ((lastKnownLocation - transform.position).sqrMagnitude < 0.1f)
+                chasing = false;
+        }
+        transform.forward = velocity.normalized;
+        controller.Move(velocity * Time.deltaTime);
 
+        //Search for player
+        if (player != null)
+        {
+            
+            Vector3 playerRelative = player.transform.position - transform.position;
+            float angleToPlayer = Mathf.Abs(Vector3.Angle(playerRelative, transform.forward));
+            print(angleToPlayer);
+            if (!Physics.Raycast(transform.position, playerRelative, playerRelative.magnitude) 
+                && angleToPlayer < viewAngle)
+            {
+                chasing = true;
+                lastKnownLocation = player.transform.position;
+            }
         }
     }
 }
