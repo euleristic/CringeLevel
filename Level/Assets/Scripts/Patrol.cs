a14257f3a316552757008f8398cc331905779254
euleristic <valter.ottenvang@gmail.com>
2021-03-03 15:53:08 +0100

    Added a lamp, patrol script and installed progrids

---
+++ Level/Assets/Scripts/Patrol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 gravity;
+    Vector3 velocity;
+    public List<Vector3> checkPoints;
+    short currentCheckPointIndex;
+    PlayerMovement player;
+    Vector3 lastKnownLocation;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        currentCheckPointIndex = 0;
+    }
+
+    void Update()
+    {
+        if (checkPoints.Count != 0)
+        {
+
+        }
+    }
+}
