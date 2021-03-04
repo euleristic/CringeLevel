8976a667cbb131546368dacc41d5b44e1602c159
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2021-03-04 10:44:37 +0100

    Ivan's commit
    -Added "TriggerBall" prefab
    -Wrote "SceneTransitioner" script that loads next scenes
    -Created "GameManager" Prefab

---
+++ Level/Assets/SceneTransitioner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("I work");
+        if (SceneManager.GetActiveScene().name == "Mattis")
+        {
+            SceneManager.LoadScene("Ivan");
+        }
+        else if (SceneManager.GetActiveScene().name == "Ivan")
+        {
+            SceneManager.LoadScene("Valter");
+        }
+        else if (SceneManager.GetActiveScene().name == "Valter")
+        {
+            SceneManager.LoadScene("Victory");
+        }
+    }
+}
