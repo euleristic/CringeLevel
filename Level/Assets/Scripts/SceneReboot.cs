c94042fcf00a0c6d49e6cc2ab131d360f364eaef
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2021-03-04 10:03:37 +0100

    Ivan's Level Upload
    -Greybox Prototype of Ivan's level
    -Restart Scene script

---
+++ Level/Assets/Scripts/SceneReboot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReboot : MonoBehaviour
+{
+    KeyCode restartKey = KeyCode.Escape;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(restartKey))
+       {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       }
+    }
+}
