f53f1ffe827f9483b1acda8a809d701251c8bca8
Daniel Cehic <danielcehic26@gmail.com>
2021-03-22 12:43:23 +0100

    Integrated button sound to button in Daniel.unity. Made so it can only be interacted with once.

--- Level/Assets/buttonScript.cs
+++ Level/Assets/buttonScript.cs
@@ -6,11 +6,15 @@ public class buttonScript : MonoBehaviour
 {
     getCaught getcaught;
     public bool pressedButton = false;
+    private bool repetitionLock = false;
+
     private void OnMouseOver()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !repetitionLock)
         {
+            repetitionLock = true;
             pressedButton = true;
+            GetComponent<AudioSource>().Play();
         }
     }
 }
