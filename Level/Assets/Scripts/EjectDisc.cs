3fe717c60e3ee5212a23a19b48fdb206fb5c756b
euleristic <valter.ottenvang@gmail.com>
2021-03-22 12:29:25 +0100

    Added sound effect when ejecting disc

--- Level/Assets/Scripts/EjectDisc.cs
+++ Level/Assets/Scripts/EjectDisc.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class EjectDisc : MonoBehaviour
 {
     [SerializeField] FloppyDisc disc;
-
+    [SerializeField] AudioSource source;
     private void OnMouseOver()
     {
-        
         if (Input.GetKeyDown(KeyCode.E))
         {
+            source.Play();
             disc.gameObject.SetActive(true);
             Destroy(gameObject);
         }
