2bfe29d7e5f0b34d474db6e994530b5c2f6020c1
euleristic <valter.ottenvang@gmail.com>
2021-03-22 11:26:58 +0100

    Made the end state interacting with a monitor

---
+++ Level/Assets/Scripts/WinInteract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinInteract : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) SceneManager.LoadScene("Victory");
+    }
+}
