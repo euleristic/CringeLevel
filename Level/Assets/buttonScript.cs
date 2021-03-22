7071ba0135ee23a1a60edc87d7e12c365e8a77c9
Daniel Cehic <danielcehic26@gmail.com>
2021-03-22 11:53:56 +0100

    Finished level
    Fixed cameras reloading scene when walking into their view. Camera view is disabled when you interact with button. Cameras have been placed in specific locations to form a path. Missing floor/ceiling materials have been fixed. Updated floor material.

---
+++ Level/Assets/buttonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour
+{
+    getCaught getcaught;
+    public bool pressedButton = false;
+    private void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pressedButton = true;
+        }
+    }
+}
